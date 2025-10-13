f4a8fb1d7c591ac9662e595848a2e3c1eb6c1827
chicoumbetov <shyngysmaaan@gmail.com>
2025-10-13 20:13:48 +0200

    :sparkles: defect context

---
+++ lib/contexts/defect-context.tsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useState } from "react";
+
+import { useApi } from "../api/roll";
+import { COLOR_MAP, mockAnalytics, mockDefects } from "../constants";
+
+interface DefectContextType {
+  analytics: string[];
+  defectsOnMap: any;
+  selectedDefectType: any;
+  setSelectedDefectType: any;
+  loading: boolean;
+  COLOR_MAP: any;
+}
+
+const DefectContext = createContext<DefectContextType | undefined>(undefined);
+
+export const DefectProvider = ({ children }: any) => {
+  const { data: analytics, loading: loadingAnalytics } = useApi("analytics", {
+    analytics: mockAnalytics,
+  });
+  const { data: defectsOnMap, loading: loadingDefects } = useApi("defects", {
+    defects: mockDefects,
+  });
+
+  const [selectedDefectType, setSelectedDefectType] = useState(null);
+
+  const value = {
+    analytics,
+    defectsOnMap,
+    selectedDefectType,
+    setSelectedDefectType,
+    loading: loadingAnalytics || loadingDefects,
+    COLOR_MAP,
+  };
+
+  return (
+    <DefectContext.Provider value={value}>{children}</DefectContext.Provider>
+  );
+};
+
+export const useDefect = () => useContext(DefectContext);
